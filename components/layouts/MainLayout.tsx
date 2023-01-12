4a13dfa8bd7be36bc1e218bce3f80ca81f7d9222
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2023-01-12 00:14:18 -0300

    Se crea el MainLayout

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import Head from "next/head";
+
+interface Props {
+  children: JSX.Element;
+  title?: string;
+}
+
+export const MainLayout: FC<Props> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>"Oscar Ariel Saucedo"</title>
+        <meta name='author' content='Oscar Ariel Saucedo' />
+        <meta
+          name='keywords'
+          content={`Oscar Ariel Saucedo, developer, react`}
+        />
+      </Head>
+
+      {/* Navbar */}
+
+      <main>{children}</main>
+    </>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import { MainLayout } from "../components/layouts";
+
 const HomePage = () => {
   return (
-    <>
+    <MainLayout>
       <h1>PortFolio Web</h1>
-    </>
+    </MainLayout>
   );
 };
 
